ae7b4d0e3deb28ef90473a36f2c8a46b29b8da91
Black-Male <Gkaranja20@gmail.com>
2023-01-23 15:04:59 +0300

    Code Challenge all deliverables done

--- src/Components/BotCollection.js
+++ src/Components/BotCollection.js
@@ -1,37 +1,35 @@
 // shows the list of bots and when clicked on a function should trigger that affects Your bot army
-import React, {useState, useEffect} from "react";
+import React from "react";
 
-function BotCollection (){
-    const [botArray, getBotArray] = useState([])
 
-    useEffect(()=>{
-        fetch("https://api.jsonbin.io/v3/b/63cd5577c0e7653a055e8a42/?meta=false")
-        .then(res => res.json())
-        .then((data) => {
-            getBotArray(data.bots)
-            console.log(data.bots)
-            console.log(data)
-        })
-    },[])
-    
+function BotCollection ({botArray, prepareToShow,deleteStuff}){ 
+
     return(
-        <div className="row mb-3">
-            {botArray.map((z)=>{
-                return (
-                    <div className="col-2">
-                        <div className="card mb-3"  key={z.id}>
-                            <img src={z.avatar_url} alt="Bot"/>
-                            <div className="card-body">
-                                <h5 className="card-title">{z.name}</h5>
-                                <p className="card-text">Health: {z.health}  Damage: {z.damage}  Armor: {z.armor}</p>
-                                <br/>
-                                <p className="card-text">{z.catchphrase}</p>
+
+            <div className="row mb-3">
+                {botArray.map((z)=>{
+                    return (
+                         
+                        <div className="col-2 mb-3" key={z.id}  id= {z.id} >
+                            <div className="card " onClick = {()=>prepareToShow(z)}  >
+                                
+                                <img src={z.avatar_url} alt="Bot"/>
+                                <div className="card-body">
+                                    <h5 className="card-title">{z.name}</h5>
+                                    <p className="card-text">{z.catchphrase}</p>
+                                    <br/>
+                                    <p className="card-text">Health: {z.health}  Damage: {z.damage}  Armor: {z.armor}</p>
+                                    
+
+                                </div>
+                                
                             </div>
+                            <button type="submit" className="btn btn-danger" onClick={()=>deleteStuff(z)}>Discharge</button>
                         </div>
-                    </div>
-                )
-            })}
-        </div>
+                    )
+                })}
+            </div>
+
     )
 }
 
